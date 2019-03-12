1763bb1b32d23466f32a94f7f842076f67fffd60
Sony Kusyana <sony.kusyana@yahoo.com>
2019-03-12 13:10:14 +0700

    index view resource

--- src/controllers/ResourceController.php
+++ src/controllers/ResourceController.php
@@ -1,13 +1,17 @@
 <?php
 namespace Gazlab\Admin\Controllers;
 
+use DataTables\DataTable;
+
 class ResourceController extends ControllerBase
 {
     // READ
     public $columns = [];
+    public $queryMethod = 'builder';
     public function column($params)
     {
         $params['header'] = isset($params['header']) ? $params['header'] : ucwords(\Phalcon\Text::humanize($params[0]));
+        $params['data'] = $params[0];
         array_push($this->columns, $params);
     }
     public function actions($extras = [])
@@ -17,8 +21,28 @@ class ResourceController extends ControllerBase
         $params['orderable'] = false;
         array_push($this->columns, $params);
     }
+    public function getQueryMethod()
+    {
+        return $this->queryMethod;
+    }
+    public function setQueryMethod($queryMethod)
+    {
+        $this->queryMethod = $queryMethod;
+    }
+    public function queryGetAll()
+    {
+        return $this->modelsManager->createBuilder()
+            ->from(\Phalcon\Text::camelize($this->router->getControllerName()));
+    }
     public function indexAction()
     {
+        if ($this->request->isAjax()) {
+            $queryMethod = 'from' . \Phalcon\Text::camelize($this->getQueryMethod());
+
+            $dataTables = new DataTable();
+            return $dataTables->$queryMethod($this->queryGetAll())->sendResponse();
+        }
+
         $this->table();
         $this->view->partial($this->config->application->viewsDir . 'contents/table', ['columns' => $this->columns, 'box' => true]);
     }

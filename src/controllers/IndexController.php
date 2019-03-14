f8f3a8278ebd924d70e757289c9a4cbc43e7e8cf
Sony Kusyana <sony.kusyana@yahoo.com>
2019-03-14 19:06:08 +0700

    fix loop redirect

--- src/controllers/IndexController.php
+++ src/controllers/IndexController.php
@@ -6,14 +6,26 @@ class IndexController extends ControllerBase
     public function indexAction()
     {
         if ($this->getArea() === 'private') {
-            return $this->dispatcher->forward([
-                'namespace' => '',
-                'controller' => $this->resources[0]->menu[0],
-            ]);
-            // return $this->response->redirect(join('/', [
-            //     $this->router->getModuleName(),
-            //     $this->resources[0]->menu[0],
-            // ]));
+            // return $this->dispatcher->forward([
+            //     'namespace' => '',
+            //     'controller' => $this->resources[0]->menu[0],
+            // ]);
+            $i = 0;
+            foreach ($this->resources as $index => $resource) {
+                if ($i === 0) {
+                    if (is_int($index)) {
+                        $resource = $resource;
+                    } else {
+                        $resource = $resource[0];
+                    }
+                    break;
+                }
+            }
+
+            return $this->response->redirect(join('/', [
+                $this->router->getModuleName(),
+                $resource->menu[0],
+            ]));
         }
         $this->dispatcher->forward([
             'controller' => 'session',

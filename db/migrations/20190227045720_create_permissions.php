4fdcd44111fcf3df18a815f3a10a326d78f53d8e
Sony Kusyana <sony.kusyana@yahoo.com>
2019-03-14 18:05:54 +0700

    migration modification

--- db/migrations/20190227045720_create_permissions.php
+++ db/migrations/20190227045720_create_permissions.php
@@ -31,7 +31,7 @@ class CreatePermissions extends AbstractMigration
      */
     public function change()
     {
-        $table = $this->table('permissions');
+        $table = $this->table('permissions', ['engine' => 'MyISAM']);
         $table->addColumn('profile_id', 'integer')
             ->addColumn('resource', 'string')
             ->addColumn('action', 'string')

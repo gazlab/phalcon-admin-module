dbde0f58a7c282e18f2debad51e7eefe9b1a45ba
Sony Kusyana <sony.kusyana@yahoo.com>
2019-08-18 10:10:04 +0700

    migration

---
+++ db/migrations/20190817105213_create_users.php
@@ -0,0 +1,45 @@
+<?php
+
+use Phinx\Migration\AbstractMigration;
+
+class CreateUsers extends AbstractMigration
+{
+    /**
+     * Change Method.
+     *
+     * Write your reversible migrations using this method.
+     *
+     * More information on writing migrations is available here:
+     * http://docs.phinx.org/en/latest/migrations.html#the-abstractmigration-class
+     *
+     * The following commands can be used in this method and Phinx will
+     * automatically reverse them when rolling back:
+     *
+     *    createTable
+     *    renameTable
+     *    addColumn
+     *    addCustomColumn
+     *    renameColumn
+     *    addIndex
+     *    addForeignKey
+     *
+     * Any other destructive changes will result in an error when trying to
+     * rollback the migration.
+     *
+     * Remember to call "create()" or "update()" and NOT "save()" when working
+     * with the Table class.
+     */
+    public function change()
+    {
+        $table = $this->table('users');
+        $table->addColumn('profile_id', 'integer')
+            ->addColumn('username', 'string')
+            ->addColumn('password', 'string')
+            ->addColumn('avatar', 'string', ['null' => true])
+            ->addColumn('blocked', 'smallinteger', ['default' => 0])
+            ->addTimestamps()
+            ->addIndex(['username'], ['unique' => true])
+            ->addForeignKey('profile_id', 'profiles', 'id')
+            ->create();
+    }
+}
---
+++ db/seeds/Users.php
@@ -0,0 +1,38 @@
+<?php
+
+
+use Phinx\Seed\AbstractSeed;
+use Phalcon\Security;
+
+class Users extends AbstractSeed
+{
+    public function getDependencies()
+    {
+        return [
+            'Profiles'
+        ];
+    }
+
+    /**
+     * Run Method.
+     *
+     * Write your database seeder using this method.
+     *
+     * More information on writing seeders is available here:
+     * http://docs.phinx.org/en/latest/seeding.html
+     */
+    public function run()
+    {
+        $security = new Security();
+        $data = [
+            [
+                'username'    => 'admin',
+                'password'  => $security->hash('admin')
+            ]
+        ];
+
+        $table = $this->table('users');
+        $table->insert($data)
+            ->save();
+    }
+}

6ada02c421043a5cd8fd41b367f615113bb6b2b4
Sony Kusyana <sony.kusyana@gmail.com>
2019-12-11 10:20:48 +0700

    set title and layouts

--- src/controllers/ControllerBase.php
+++ src/controllers/ControllerBase.php
@@ -3,4 +3,11 @@
 namespace Gazlab\Admin\Controllers;
 
 class ControllerBase extends \Phalcon\Mvc\Controller
-{ }
+{ 
+    public function initialize()
+    {
+        $this->tag->setTitle('Gazlab Admin');
+
+        $this->view->setTemplateBefore('private');
+    }
+}
